c2e51dea5678f6a2dfb63e2a40dd0837cebe493f
Gajanan Gaikwad <gajanan.gaikwad@tele2.com>
2019-06-21 15:49:09 +0300

    First Commit Setup Of Spring boot

---
+++ src/main/java/com/cipher/CipherRobotApplication.java
@@ -0,0 +1,13 @@
+package com.cipher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CipherRobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CipherRobotApplication.class, args);
+	}
+
+}
