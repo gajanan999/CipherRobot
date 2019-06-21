5e50e9b6fac4601cdd3d90cfdc27f206cbf2fa06
Gajanan Gaikwad <gajanan.gaikwad@tele2.com>
2019-06-21 15:59:39 +0300

    Added the controller, service, adn repository class

---
+++ src/main/java/com/cipher/controller/MainController.java
@@ -0,0 +1,8 @@
+package com.cipher.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+
+}
---
+++ src/main/java/com/cipher/dao/DataRepository.java
@@ -0,0 +1,8 @@
+package com.cipher.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DataRepository {
+
+}
